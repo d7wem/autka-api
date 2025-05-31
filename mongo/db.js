31d74217828076ae7987caa2674b2296522ada9f
d7wem <107113371+d7wem@users.noreply.github.com>
2025-05-31 16:14:49 +0300

    Update db.js

--- mongo/db.js
+++ mongo/db.js
@@ -1,15 +1,12 @@
 // mongo/db.js
 const mongoose = require('mongoose');
-const { dbUrl } = require('../config');
-
-mongoose.Promise = global.Promise;
+const { dbUrl } = require('../config');     // ← ключ збігається
 
 module.exports = () => {
-  mongoose
-    .connect(dbUrl, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    })
-    .then(() => console.log('Successfully connected to db'))
-    .catch(err => console.error('Mongo error:', err.message));
+  mongoose.connect(dbUrl, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => console.log('Successfully connected to db'))
+  .catch(err => console.error('Mongo error:', err.message));
 };
