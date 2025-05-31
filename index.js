7e71393e9de334cacbecb57a3c554ee02fa7b586
d7wem <107113371+d7wem@users.noreply.github.com>
2025-05-31 16:10:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// index.js
 const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
@@ -5,15 +6,13 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors());
-
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-require('./mongo/db')();
-
+require('./mongo/db')();                 // підключення до Mongo
 require('./api/routes/lot.routes')(app);
 require('./api/routes/make.routes')(app);
 
-app.listen(3001, () => {
-  console.log('listening on 3001');
-});
+// ---- Головна зміна ↓
+const PORT = process.env.PORT || 3001;
+app.listen(PORT, '0.0.0.0', () => console.log(`listening on ${PORT}`));
